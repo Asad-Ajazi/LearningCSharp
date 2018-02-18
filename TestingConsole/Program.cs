aa09e1ad714114a8d671b410ecb6266f0102ac3c
Asad Ajazi <AsadAjazi.work@gmail.com>
2018-02-18 19:20:39 +0000

    Added testing console

---
+++ TestingConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingConsole //will be used to test small sections of code.
+{
+
+    class Account
+    {
+        public string name;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account RobA = new Account();
+            RobA.name = "rob";
+            Console.WriteLine(RobA.name);
+
+            RobA = new Account();
+            RobA.name = "tempjim";
+            Console.WriteLine(RobA.name);
+
+        }
+    }
+}
