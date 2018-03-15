6d00dac2ab63b5a002e89337f64364f665bd57f9
Asad Ajazi <AsadAjazi.work@gmail.com>
2018-03-15 10:28:57 +0000

    begging to modify the BankApp and begin putting all concepts learnt into an application

---
+++ BankApp/ArrayBank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class ArrayBank
+    {
+        private IAccount[] accounts;
+
+        public ArrayBank(int bankSize)
+        {
+            accounts = new IAccount[bankSize];
+        }
+    }
+}
