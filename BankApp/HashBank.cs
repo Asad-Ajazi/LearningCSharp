fe9d857c31008eadbe35ab7c9d7b4fefc3b6130f
Asad Ajazi <AsadAjazi.work@gmail.com>
2018-03-24 17:35:24 +0000

    finished section 4, using arrays and hashtables. Now moving onto sectoin 5 advanced programming

---
+++ BankApp/HashBank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class HashBank : IBank
+    {
+        Hashtable bankHashtable = new Hashtable();
+
+        public IAccount FindAccount(string name)
+        {
+            return bankHashtable[name] as IAccount;
+        }
+
+        public bool StoreAccount(IAccount account)
+        {
+            bankHashtable.Add(account.GetName(), account);
+            return true;
+        }
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -12,6 +12,8 @@ namespace BankApp
         static void Main(string[] args)
         {
 
+            
+
             ArrayBank ourbank = new ArrayBank(100);
             IAccount newAccount = new CustomerAccount("Rob","add",10000);
             IAccount anewAccount = new CustomerAccount("Tom", "add", 10000);
@@ -26,11 +28,27 @@ namespace BankApp
                 Console.WriteLine("account added");
             }
 
-            IAccount storedAccount = ourbank.FindAccount("Tam");
+            IAccount storedAccount = ourbank.FindAccount("Rob");
             if (storedAccount!=null)
             {
                 Console.WriteLine("Account found in bank");
             }
+
+            //testing hashtables.
+            HashBank hbank = new HashBank();
+
+            IAccount jimAcc = new CustomerAccount("Jim", "add", 10000);
+            IAccount joeAcc = new CustomerAccount("Joe", "housing place", 400);
+
+            hbank.StoreAccount(jimAcc);
+            hbank.StoreAccount(joeAcc);
+
+            IAccount hashAccount = hbank.FindAccount("Jim");
+            if (hashAccount != null)
+            {
+                Console.WriteLine("hash Account found in bank");
+            }
+
         }
     }
 }
